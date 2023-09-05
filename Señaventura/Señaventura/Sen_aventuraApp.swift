e1b403266af5d0e36b32a684b4fa017825670ee8
Christopher Pedraza Pohlenz <62347713+christopher-pedraza@users.noreply.github.com>
2023-09-05 15:23:09 -0600

    Add files via upload

---
+++ Señaventura/Señaventura/Sen_aventuraApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sen_aventuraApp.swift
+//  Señaventura
+//
+//  Created by Alumno on 05/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Sen_aventuraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
