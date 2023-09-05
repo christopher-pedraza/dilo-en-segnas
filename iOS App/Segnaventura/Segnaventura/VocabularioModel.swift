870b416781f284303363c807b604340de8ed4187
Christopher Pedraza Pohlenz <62347713+christopher-pedraza@users.noreply.github.com>
2023-09-05 16:24:39 -0600

    Actualizar json y crear Model para vocabulario

---
+++ iOS App/Segnaventura/Segnaventura/VocabularioModel.swift
@@ -0,0 +1,44 @@
+//
+//  VocabularioModel.swift
+//  Segnaventura
+//
+//  Created by Alumno on 05/09/23.
+//
+
+import Foundation
+
+struct VocabularioModel : Decodable, Identifiable {
+    var id = UUID()
+    var categorias : [Categorias]
+    
+    enum CodingKeys : String, CodingKey {
+        case categorias
+    }
+    
+}
+
+struct Categorias: Decodable, Identifiable {
+    var id = UUID()
+    var nombre_categoria : String
+    var vocabulario : [Vocabulario]
+    
+    enum CodingKeys : String, CodingKey {
+        case nombre_categoria
+        case vocabulario
+    }
+}
+
+struct Vocabulario: Decodable, Identifiable {
+    var id = UUID()
+    var palabra_espagnol : String
+    var url_audio : String
+    var url_video : String
+    var url_imagen : String
+    
+    enum CodingKeys : String, CodingKey {
+        case palabra_espagnol
+        case url_audio
+        case url_video
+        case url_imagen
+    }
+}
