670b91073baa4c77f58cdd5d9fb7074f1abe7435
David-Cavazos <88981757+David-Cavazos@users.noreply.github.com>
2023-09-06 19:38:34 -0600

    Avance para album de insignias
    Avance la parte visual de como esta organizadas las insignias por catalogo. Actualmente tiene un botón de show more para las insignias que no caben en el catalogo, esperando poder hacer un botón como el de iOS para expandir y poder ver el resto de las insignias.

---
+++ iOS App/iosFolders/iosFolders/iosFoldersApp.swift
@@ -0,0 +1,19 @@
+//
+//  iosFoldersApp.swift
+//  iosFolders
+//
+//  Created by David E Cavazos on 06/09/23.
+//
+
+import SwiftUI
+
+@main
+struct iosFoldersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        
+        
+    }
+}
