e384ac58c1e59a58044c20db16b92e93ddbf69f1
Fausto <faustop.mtz27@gmail.com>
2023-09-21 14:53:54 -0600

    Ajustar colores y botones

--- iOS App/Segnaventura/Segnaventura/Foundation copy/Views/ContentView.swift
+++ iOS App/Segnaventura/Segnaventura/Foundation copy/Views/ContentView.swift
@@ -11,19 +11,24 @@ import AVFoundation
 struct ContentView: View {
     var body: some View {
         // Tab view con las dos demos de las funcionalidades
-        TabView {
-            VocabularioDataDemo()
-                .tabItem {
-                    Label("VocabularioDataDemo", systemImage: "gearshape.2.fill")}
-            FileManagerDemo()
-                .tabItem {
-                    Label("FileManagerDemo", systemImage: "gearshape.2.fill")
-                }
-            LaunchScreenView()
-                .tabItem {
-                    Label("Scanner", systemImage: "gearshape.2.fill")
-                }
+        NavigationView{
+            
+
+            TabView {
+                VocabularioDataDemo()
+                    .tabItem {
+                        Label("VocabularioDataDemo", systemImage: "gearshape.2.fill")}
+                FileManagerDemo()
+                    .tabItem {
+                        Label("FileManagerDemo", systemImage: "gearshape.2.fill")
+                    }
+                LaunchScreenView()
+                    .tabItem {
+                        Label("Scanner", systemImage: "gearshape.2.fill")
+                    }
+            }
         }
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
