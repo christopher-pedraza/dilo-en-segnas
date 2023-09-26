ce07c515bd358fc1dda2d449024e3e9c6242c807
Adrián Ramírez <adrian.rmz.587@gmail.com>
2023-09-26 09:48:13 -0600

    Added COML FIles

---
+++ iOS App/Segnaventura/Segnaventura/2-Data/Classification.swift
@@ -0,0 +1,14 @@
+//
+//  Classification.swift
+//  coreML-starter
+//
+//
+//
+
+struct Classification: Decodable {
+    // TODO: replace with the name of your keys from mydata.json and some default values
+    var label: String = "Pan"
+    var image: String = "pan"
+    var video: String = "pan"
+    var audio: String = "pan.m4a"
+}
