e162259097cb53b34b1097f0fab9c5741680dddc
Adrián Ramírez <adrian.rmz.587@gmail.com>
2023-09-29 23:06:40 -0600

    Added Quiz Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import CategoriesPage from "./pages/CategoriesPage";
 import WordsPage from "./pages/WordsPage";
+import QuizPage from "./pages/QuizPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route index element={<CategoriesPage />} />
         <Route path="/categories" element={<CategoriesPage />} />
         <Route path="/categories/:id_category" element={<WordsPage />} />
+        <Route path="/quiz" element={<QuizPage />} />
       </Routes>
     </BrowserRouter>
   );
