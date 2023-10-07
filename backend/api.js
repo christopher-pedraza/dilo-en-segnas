aa80733dc732e523a6b98240b09288c77652b5d9
Adrián Ramírez <adrian.rmz.587@gmail.com>
2023-10-06 19:31:20 -0600

    Added CRD Categories

--- backend/api.js
+++ backend/api.js
@@ -8,21 +8,26 @@ npx prisma init
 npm install @prisma/client
 */
 
-const express = require('express')
-const dotenv = require('dotenv')
-dotenv.config()
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
 
-const app = express()
+dotenv.config();
+
+const app = express();
+
+// Habilitar CORS para todas las rutas
+app.use(cors());
 
 // Para poder trabajar con JSONS
-app.use(express.json())
+app.use(express.json());
 // Para escribir datos en el body al hacer post o put
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
 // Para poder usar todas las rutas
-app.use(require('./routes/routes.js'))
+app.use(require("./routes/routes.js"));
 
 // Se inicializa la API en el puerto especificado por el archivo .env
 app.listen(process.env.PORT, () => {
-    console.log(`Listening on port ${process.env.PORT}`)
-})
+  console.log(`Listening on port ${process.env.PORT}`);
+});
