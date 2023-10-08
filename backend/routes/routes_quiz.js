8baee4789b944385b13f0299599d57c2ebbc34b7
Christopher Pedraza <gabalixreborn@gmail.com>
2023-10-08 14:20:38 -0600

    Actualizar endpoints de quiz

--- backend/routes/routes_quiz.js
+++ backend/routes/routes_quiz.js
@@ -5,12 +5,13 @@ const controller = require('../controllers/controller_quiz.js')
 // Rutas disponibles y las funciones que se llaman
 router.get('/getAll', controller.getAll)
 router.get('/get/:id', controller.get)
+router.get('/getPalabrasByQuiz/:id', controller.getPalabrasByQuiz)
+router.get('/getComplete/:id', controller.getComplete)
 router.post('/add', controller.add)
 router.post('/addConPalabras', controller.addConPalabras)
 router.delete('/remove/:id', controller.remove)
 router.put('/update/:id', controller.update)
 router.post('/addPalabra/:id', controller.addPalabra)
 router.delete('/removePalabra/:id', controller.removePalabra)
-router.get('/getPalabrasByQuiz/:id', controller.getPalabrasByQuiz)
 
 module.exports = router
