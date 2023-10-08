c485b9f82c9e3ff58f764fb33e63f172c32cad04
Adrián Ramírez <adrian.rmz.587@gmail.com>
2023-10-08 00:28:12 -0600

    Added Create & Read Quiz

--- frontend/src/pages/QuizPage.jsx
+++ frontend/src/pages/QuizPage.jsx
@@ -1,17 +1,125 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Dialog } from "@headlessui/react";
+import axios from "axios";
 
 import Navbar from "../components/Navbar";
-import ItemCategory from "../components/ItemCategory";
+import ItemQuiz from "../components/ItemQuiz";
 import ItemToggle from "../components/ItemToggle";
 
 export default function QuizPage() {
-  let [isOpen, setIsOpen] = useState(false);
+  // Estados para controlar si el modal esta abierto o cerrado
+  let [isOpenCreate, setIsOpenCreate] = useState(false);
+  let [isOpenUpdate, setIsOpenUpdate] = useState(false);
+
+  // Estado para controlar el formulario
+  const [formData, setFormData] = useState({
+    id_isla: "default",
+    nombre: "",
+    palabras: [],
+  });
+
+  // Estado para controlar los quizes
+  const [quizes, setQuizes] = useState([]);
+
+  // Estado para controlar las categorias
+  const [categories, setCategories] = useState([]);
+
+  // Estado para controlar las palabras
+  const [words, setWords] = useState([]);
+
+  // Estado para controlar el ID del quiz a editar
+  const [editID, setEditID] = useState();
+
+  // Estado para controlar el refresh de la pagina
+  const [refresh, setRefresh] = useState(0);
+
+  // Destructuring del formData
+  const { id_isla, nombre, palabras } = formData;
+
+  // Funcion que se ejecuta cada vez que se cambia el valor de un input
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  // Funcion que se ejecuta cuando se da click en el boton de crear
+  const handleCreate = (e) => {
+    e.preventDefault();
+    if (id_isla && nombre && palabras) {
+      axios
+        .post("http://localhost:3000/quiz/addConPalabras", formData)
+        .then((res) => {
+          setQuizes([...quizes, res.data]);
+          setFormData({ id_isla: "default", nombre: "", palabras: [] });
+        })
+        .catch((err) => console.log(err));
+    }
+    setIsOpenCreate(false);
+  };
+
+  // Funcion que obtiene todos los quizes y los actualiza cada que cambia el estado refresh
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/quiz/getAll")
+      .then((res) => {
+        setQuizes(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, [refresh]);
+
+  // Funcion que obtiene todas las categorias
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/categorias/getAll")
+      .then((res) => {
+        setCategories(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  // Funcion que obtiene todas las palabras de una categoría
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3000/palabras/getByCategoria/${formData.id_isla}`)
+      .then((res) => {
+        setWords(res.data);
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, [formData.id_isla]);
+
+  // Función para manejar la selección/deselección de palabras
+  const handleToggle = (palabraId, isSelected) => {
+    const updatedPalabras = formData.palabras.slice(); // Crear una copia del array de palabras
+
+    if (isSelected) {
+      // Agregar la palabra a las palabras seleccionadas
+      updatedPalabras.push({ id_palabra: palabraId });
+    } else {
+      // Remover la palabra de las palabras seleccionadas
+      const indexToRemove = updatedPalabras.findIndex(
+        (p) => p.id_palabra === palabraId
+      );
+      updatedPalabras.splice(indexToRemove, 1);
+    }
+
+    setFormData({
+      ...formData,
+      palabras: updatedPalabras,
+    });
+  };
+
+  // Efecto para reiniciar las palabras cuando cambia la categoría
+  useEffect(() => {
+    setFormData({
+      ...formData,
+      palabras: [],
+    });
+  }, [formData.id_isla]);
 
   return (
     <>
-      <Dialog open={isOpen} onClose={() => setIsOpen(false)}>
-        <Dialog open={isOpen} onClose={() => setIsOpen(false)}>
+      <Dialog open={isOpenCreate} onClose={() => setIsOpenCreate(false)}>
+        <Dialog open={isOpenCreate} onClose={() => setIsOpenCreate(false)}>
           {/* The backdrop, rendered as a fixed sibling to the panel container */}
           <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
 
@@ -20,45 +128,53 @@ export default function QuizPage() {
             {/* The actual dialog panel  */}
             <Dialog.Panel className="flex w-full max-w-md flex-col rounded-lg bg-white p-6">
               <form>
-                <label htmlFor="quizName" className="text-2xl font-bold">
+                <label htmlFor="nombre" className="text-2xl font-bold">
                   Nombre
                 </label>
                 <input
                   type="text"
-                  id="quizName"
-                  name="quizName"
+                  id="nombre"
+                  name="nombre"
                   placeholder="Ingresa el nombre del quiz"
                   className="w-full rounded-lg border border-slate-400 p-2 my-2"
+                  value={nombre}
+                  onChange={handleChange}
                 />
-                <label htmlFor="quizCategory" className="text-2xl font-bold">
+                <label htmlFor="id_isla" className="text-2xl font-bold">
                   Categoría
                   <select
-                    name="quizCategory"
-                    id="quizCategory"
-                    defaultValue="default"
+                    name="id_isla"
+                    id="id_isla"
                     className="w-full rounded-lg border border-slate-400 p-2 my-2 text-base font-normal inactive"
+                    value={id_isla}
+                    onChange={handleChange}
                   >
                     <option value="default" disabled>
                       --- Selecciona una categoria ---
                     </option>
-                    <option value="comidas">Comidas</option>
-                    <option value="naturaleza">Naturaleza</option>
-                    <option value="colores">Colores</option>
+                    {categories.map((category, index) => (
+                      <option key={index} value={category.id_isla}>
+                        {category.nombre}
+                      </option>
+                    ))}
                   </select>
                 </label>
-                <label htmlFor="quizPalabras" className="text-2xl font-bold">
+                <label htmlFor="palabras" className="text-2xl font-bold">
                   Palabras
                 </label>
                 <div className="mb-6">
-                  <ItemToggle />
-                  <ItemToggle />
-                  <ItemToggle />
-                  <ItemToggle />
+                  {words.map((word, index) => (
+                    <ItemToggle
+                      key={index}
+                      data={word}
+                      onToggle={handleToggle}
+                    />
+                  ))}
                 </div>
                 <div className="flex items-center justify-end mt-1">
                   <button
                     className="rounded-lg border border-slate-400 bg-black px-4 py-2 text-white ml-2"
-                    onClick={() => setIsOpen(false)}
+                    onClick={() => setIsOpenCreate(false)}
                   >
                     Cancelar
                   </button>
@@ -66,7 +182,7 @@ export default function QuizPage() {
                     className="rounded-lg border border-slate-400 px-4 py-2 text-white ml-2"
                     style={{ background: "#8712E0" }}
                     type="submit"
-                    onClick={() => setIsOpen(false)}
+                    onClick={handleCreate}
                   >
                     Crear
                   </button>
@@ -82,7 +198,7 @@ export default function QuizPage() {
         <div className="flex items-center justify-between">
           <h2 className="text-3xl">Quiz</h2>
           <button
-            onClick={() => setIsOpen(true)}
+            onClick={() => setIsOpenCreate(true)}
             className="p-2 text-white rounded-md"
             style={{ background: "#8712E0" }}
           >
@@ -90,9 +206,16 @@ export default function QuizPage() {
           </button>
         </div>
         <div>
-          <ItemCategory />
-          <ItemCategory />
-          <ItemCategory />
+          {/* Map que muestra cada quiz del array quizes */}
+          {quizes.map((quiz, index) => (
+            <ItemQuiz
+              key={index}
+              data={quiz}
+              // onDelete={handleDelete}
+              // onEdit={handleEdit}
+              // onSetEditID={handleSetEditID}
+            />
+          ))}
         </div>
       </div>
     </>
