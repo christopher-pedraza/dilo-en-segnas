b5988c244df81229dbf73fe87081f03ca37e57b8
Christopher Pedraza <gabalixreborn@gmail.com>
2023-10-12 14:23:04 -0600

    Eliminar migrations extra

--- backend/prisma/migrations/0_init/migration.sql
+++ backend/prisma/migrations/0_init/migration.sql
@@ -12,8 +12,8 @@ CREATE TABLE "miembro" (
 CREATE TABLE "isla" (
     "id_isla" SERIAL NOT NULL,
     "nombre" TEXT NOT NULL,
-    "modelo_general" BYTEA NOT NULL,
-    "modelo_especifico" BYTEA NOT NULL,
+    "modelo_general" BYTEA,
+    "modelo_especifico" BYTEA,
 
     CONSTRAINT "isla_pkey" PRIMARY KEY ("id_isla")
 );
@@ -21,6 +21,7 @@ CREATE TABLE "isla" (
 -- CreateTable
 CREATE TABLE "video_cuestionario" (
     "id_video_cuestionario" SERIAL NOT NULL,
+    "nombre" TEXT NOT NULL,
     "id_isla" INTEGER NOT NULL,
 
     CONSTRAINT "video_cuestionario_pkey" PRIMARY KEY ("id_video_cuestionario")
@@ -30,6 +31,7 @@ CREATE TABLE "video_cuestionario" (
 CREATE TABLE "quiz" (
     "id_quiz" SERIAL NOT NULL,
     "id_isla" INTEGER NOT NULL,
+    "nombre" TEXT NOT NULL,
 
     CONSTRAINT "quiz_pkey" PRIMARY KEY ("id_quiz")
 );
@@ -77,16 +79,19 @@ CREATE TABLE "palabra" (
 CREATE TABLE "parte_video_cuestionario" (
     "id_parte_video_cuestionario" SERIAL NOT NULL,
     "id_video_cuestionario" INTEGER NOT NULL,
+    "url_video" TEXT NOT NULL,
+    "indice" INTEGER NOT NULL,
+    "nombre" TEXT NOT NULL,
 
     CONSTRAINT "parte_video_cuestionario_pkey" PRIMARY KEY ("id_parte_video_cuestionario")
 );
 
 -- CreateTable
 CREATE TABLE "palabras_video_cuestionario" (
-    "id_parte_video_cuestionario" INTEGER NOT NULL,
+    "id_video_cuestionario" INTEGER NOT NULL,
     "id_palabra" INTEGER NOT NULL,
 
-    CONSTRAINT "palabras_video_cuestionario_pkey" PRIMARY KEY ("id_parte_video_cuestionario","id_palabra")
+    CONSTRAINT "palabras_video_cuestionario_pkey" PRIMARY KEY ("id_video_cuestionario","id_palabra")
 );
 
 -- CreateTable
@@ -101,7 +106,7 @@ CREATE TABLE "preguntas_video_cuestionario" (
 -- CreateTable
 CREATE TABLE "respuestas_video_cuestionario" (
     "id_respuestas_video_cuestionario" SERIAL NOT NULL,
-    "id_parte_video_cuestionario" INTEGER NOT NULL,
+    "id_preguntas_video_cuestionario" INTEGER NOT NULL,
     "respuesta" TEXT NOT NULL,
     "es_correcta" BOOLEAN NOT NULL,
 
@@ -147,23 +152,23 @@ ALTER TABLE "nivel" ADD CONSTRAINT "nivel_id_treasure_hunt_fkey" FOREIGN KEY ("i
 ALTER TABLE "palabra" ADD CONSTRAINT "palabra_id_isla_fkey" FOREIGN KEY ("id_isla") REFERENCES "isla"("id_isla") ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 -- AddForeignKey
-ALTER TABLE "parte_video_cuestionario" ADD CONSTRAINT "parte_video_cuestionario_id_video_cuestionario_fkey" FOREIGN KEY ("id_video_cuestionario") REFERENCES "video_cuestionario"("id_video_cuestionario") ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE "parte_video_cuestionario" ADD CONSTRAINT "parte_video_cuestionario_id_video_cuestionario_fkey" FOREIGN KEY ("id_video_cuestionario") REFERENCES "video_cuestionario"("id_video_cuestionario") ON DELETE CASCADE ON UPDATE NO ACTION;
 
 -- AddForeignKey
-ALTER TABLE "palabras_video_cuestionario" ADD CONSTRAINT "palabras_video_cuestionario_id_parte_video_cuestionario_fkey" FOREIGN KEY ("id_parte_video_cuestionario") REFERENCES "parte_video_cuestionario"("id_parte_video_cuestionario") ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE "palabras_video_cuestionario" ADD CONSTRAINT "palabras_video_cuestionario_id_video_cuestionario_fkey" FOREIGN KEY ("id_video_cuestionario") REFERENCES "video_cuestionario"("id_video_cuestionario") ON DELETE CASCADE ON UPDATE NO ACTION;
 
 -- AddForeignKey
-ALTER TABLE "palabras_video_cuestionario" ADD CONSTRAINT "palabras_video_cuestionario_id_palabra_fkey" FOREIGN KEY ("id_palabra") REFERENCES "palabra"("id_palabra") ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE "palabras_video_cuestionario" ADD CONSTRAINT "palabras_video_cuestionario_id_palabra_fkey" FOREIGN KEY ("id_palabra") REFERENCES "palabra"("id_palabra") ON DELETE CASCADE ON UPDATE NO ACTION;
 
 -- AddForeignKey
-ALTER TABLE "preguntas_video_cuestionario" ADD CONSTRAINT "preguntas_video_cuestionario_id_parte_video_cuestionario_fkey" FOREIGN KEY ("id_parte_video_cuestionario") REFERENCES "parte_video_cuestionario"("id_parte_video_cuestionario") ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE "preguntas_video_cuestionario" ADD CONSTRAINT "preguntas_video_cuestionario_id_parte_video_cuestionario_fkey" FOREIGN KEY ("id_parte_video_cuestionario") REFERENCES "parte_video_cuestionario"("id_parte_video_cuestionario") ON DELETE CASCADE ON UPDATE NO ACTION;
 
 -- AddForeignKey
-ALTER TABLE "respuestas_video_cuestionario" ADD CONSTRAINT "respuestas_video_cuestionario_id_parte_video_cuestionario_fkey" FOREIGN KEY ("id_parte_video_cuestionario") REFERENCES "parte_video_cuestionario"("id_parte_video_cuestionario") ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE "respuestas_video_cuestionario" ADD CONSTRAINT "respuestas_video_cuestionario_id_preguntas_video_cuestiona_fkey" FOREIGN KEY ("id_preguntas_video_cuestionario") REFERENCES "preguntas_video_cuestionario"("id_preguntas_video_cuestionario") ON DELETE CASCADE ON UPDATE NO ACTION;
 
 -- AddForeignKey
-ALTER TABLE "detalles_quiz" ADD CONSTRAINT "detalles_quiz_id_quiz_fkey" FOREIGN KEY ("id_quiz") REFERENCES "quiz"("id_quiz") ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE "detalles_quiz" ADD CONSTRAINT "detalles_quiz_id_quiz_fkey" FOREIGN KEY ("id_quiz") REFERENCES "quiz"("id_quiz") ON DELETE CASCADE ON UPDATE NO ACTION;
 
 -- AddForeignKey
-ALTER TABLE "detalles_quiz" ADD CONSTRAINT "detalles_quiz_id_palabra_fkey" FOREIGN KEY ("id_palabra") REFERENCES "palabra"("id_palabra") ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE "detalles_quiz" ADD CONSTRAINT "detalles_quiz_id_palabra_fkey" FOREIGN KEY ("id_palabra") REFERENCES "palabra"("id_palabra") ON DELETE CASCADE ON UPDATE NO ACTION;
 
