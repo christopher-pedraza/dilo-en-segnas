0c11d8f4fe8fb11fe25f086388cfd926a3897341
Adrián Ramírez <adrian.rmz.587@gmail.com>
2023-10-17 22:25:11 -0600

    Completed ProgresoModel

--- iOS App/Segnaventura/Segnaventura/Progreso/ProgresoModel.swift
+++ iOS App/Segnaventura/Segnaventura/Progreso/ProgresoModel.swift
@@ -8,18 +8,62 @@
 import Foundation
 
 // Modelo para obtener el progreso de islas descubiertas
-struct ProgresoModel : Decodable, Identifiable {
+struct ProgresoModel: Decodable, Identifiable {
     var id = UUID()
-    var id_miembro : Int = 0
-    var id_isla : Int = 0
-    var isla : Isla = Isla()
+    var id_miembro: Int = 0
+    var id_isla: Int = 0
+    var isla: Isla = Isla()
 }
 
-struct Isla : Decodable, Identifiable {
+struct Isla: Decodable, Identifiable {
     var id = UUID()
+    var id_isla: Int = 0
+    var nombre: String = ""
+    var modelo_general: String = ""
+    var modelo_especifico: String = ""
+    var nivel: [Nivel] = [Nivel]()
+}
+
+struct Nivel : Decodable, Identifiable {
+    var id = UUID()
+    var id_nivel : Int = 0
+    var id_isla : Int = 0
+    var id_video_cuestionario : Int = 0
+    var id_quiz : Int = 0
+    var id_treasure_hunt : Int = 0
+    var quiz : Quiz = Quiz()
+    var treasure_hunt : TreasureHunt = TreasureHunt()
+    var video_cuestionario : VideoCuestionario = VideoCuestionario()
+    var progreso_nivel : [ProgresoNivel] = [ProgresoNivel]()
+}
+
+
+struct Quiz : Decodable, Identifiable {
+    var id = UUID()
+    var id_quiz : Int = 0
     var id_isla : Int = 0
     var nombre : String = ""
-    var modelo_general : String = ""
-    var modelo_especifico : String = ""
-    var nivel : [Int] = []
+}
+
+struct TreasureHunt : Decodable, Identifiable {
+    var id = UUID()
+    var id_treasure_hunt : Int = 0
+    var id_isla : Int = 0
+    var id_modelo_coml : Int = 0
+}
+
+struct VideoCuestionario : Decodable, Identifiable {
+    var id = UUID()
+    var id_video_cuestionario : Int = 0
+    var nombre : String = ""
+    var id_isla : Int = 0
+}
+
+struct ProgresoNivel : Decodable, Identifiable {
+    var id = UUID()
+    var id_miembro : Int = 0
+    var id_nivel : Int = 0
+    var completada_treasure_hunt : Bool = false
+    var completada_videos_cuestionario : Bool = false
+    var completada_quiz : Bool = false
 }
