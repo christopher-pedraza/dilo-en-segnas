5da25d82586ed70751fa14b6168197ff2e0e6e37
David-Cavazos <88981757+David-Cavazos@users.noreply.github.com>
2023-10-17 22:13:11 -0600

    Add files via upload

---
+++ iOS App/Segnaventura/Segnaventura/Segnaventura/PalabrasVideos/PalabrasVideos/PalabrasVideosViewModel.swift
@@ -0,0 +1,53 @@
+//
+//  PalabrasVideosViewModel.swift
+//  Segnaventura
+//
+//  Created by David E Cavazos on 28/09/23.
+//
+
+import Foundation
+
+class PalabraVideosViewModel : ObservableObject {
+    // Estructura para deserializar el JSON
+    //@Published var palabras_videos = PalabrasVideosModel()
+    @Published var palabras = [PalabrasVideosModel]()
+    
+    
+    // Funcion para leer un JSON de un API en linea y desearizarlo para poder guardarlo
+    // en un objeto que se pueda luego usar en la aplicacion
+    func getPalabrasVideos() async throws {
+        // Guarda el URL donde esta almacenado el JSON
+        guard let url = URL(string: "http://localhost:3000/quiz/getPalabrasByQuiz/1")
+                else {
+                    return
+                }
+            
+            //  Realiza un request al URL
+            let urlRequest = URLRequest(url: url)
+        do{
+            // Obtiene los datos del request
+            let (data, response) = try await URLSession.shared.data(for: urlRequest)
+            // Si es que hubo un error en el request
+            guard (response as? HTTPURLResponse)?.statusCode == 200 else {
+                print("Error: HTTP Request Failed")
+                return
+            }
+            
+            // Los datos obtenidos del API se decodifican usando la estructura en el VocabularioModel
+            // Para crear un objeto de VocabularioModel
+            let results = try JSONDecoder().decode([PalabrasVideosModel].self, from: data)
+            DispatchQueue.main.async {
+                self.palabras = results
+            }
+            
+           
+            print("Data fetched successfully")
+        }
+        catch {
+                print("Error: \(error)")
+            }
+        
+            
+    }
+}
+
