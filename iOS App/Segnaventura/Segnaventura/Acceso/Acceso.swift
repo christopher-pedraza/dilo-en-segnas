6d9cbd13ef648c02875099290057c199c0e1e5af
Alumno <62347713+christopher-pedraza@users.noreply.github.com>
2023-10-18 18:04:45 -0600

    Merge manual de cambios

---
+++ iOS App/Segnaventura/Segnaventura/Acceso/Acceso.swift
@@ -0,0 +1,163 @@
+//
+//  Login.swift
+//  Segnaventura
+//
+//  Created by Alumno on 16/10/23.
+//
+
+import SwiftUI
+import ActionButton
+import TogglableSecureField
+
+struct Acceso: View {
+    @EnvironmentObject var AccesoVM : AccesoViewModel
+    
+    var body: some View {
+        if AccesoVM.accesoValido {
+            ContentView()
+        } else {
+            TabView {
+                Login()
+                Register()
+            }
+            .tabViewStyle(.page(indexDisplayMode: .always))
+            .indexViewStyle(.page(backgroundDisplayMode: .always))
+        }
+    }
+}
+
+enum FocusableField: Hashable {
+    case user, password
+}
+
+struct Login: View {
+    @EnvironmentObject var AccesoVM : AccesoViewModel
+    @State private var isPerformingTask = false
+    @FocusState private var focus: FocusableField?
+    
+    var body: some View {
+        ZStack {
+            Image("Wallpaper_azul")
+                .centerCropped()
+            Circle()
+                .scale(0.9)
+                .foregroundColor(.white.opacity(0.15))
+            Circle()
+                .scale(0.7)
+                .foregroundColor(.white.opacity(0.90))
+            VStack {
+                Text("Inicio de sesión")
+                    .font(.largeTitle)
+                    .bold()
+                    .padding()
+                TextField("Usuario", text: $AccesoVM.user)
+                    .padding()
+                    .frame(width: 300, height: 50)
+                    .background(Color.black.opacity(0.05))
+                    .cornerRadius(10)
+                    .autocorrectionDisabled()
+                    .textInputAutocapitalization(.never)
+                    .submitLabel(.next)
+                    .focused($focus, equals: .user)
+                    .onSubmit {
+                        focus = .password
+                    }
+                TogglableSecureField("Contraseña",
+                                     secureContent: $AccesoVM.password,
+                                     onCommit: {
+                    guard !AccesoVM.password.isEmpty else { return }
+                })
+                .padding()
+                .frame(width: 300, height: 50)
+                .background(Color.black.opacity(0.05))
+                .cornerRadius(10)
+                .focused($focus, equals: .password)
+                .submitLabel(.go)
+                .onSubmit {
+                    Task {
+                        await autenticarUsuario()
+                    }
+                }
+                ActionButton(state: $AccesoVM.buttonState, onTap: {
+                    Task {
+                        await autenticarUsuario()
+                    }
+                }, backgroundColor: .blue)
+                .frame(width: 300, height: 50)
+            }
+        }
+    }
+    
+    func autenticarUsuario() async {
+        do {
+            try await AccesoVM.validarAcceso()
+        } catch {
+        }
+    }
+}
+
+struct Register: View {
+    @EnvironmentObject var AccesoVM : AccesoViewModel
+    @State private var isPerformingTask = false
+    @FocusState private var focus: FocusableField?
+    
+    var body: some View {
+        ZStack {
+            Image("Wallpaper_azul_claro")
+                .centerCropped()
+            Circle()
+                .scale(0.9)
+                .foregroundColor(.white.opacity(0.15))
+            Circle()
+                .scale(0.7)
+                .foregroundColor(.white.opacity(0.90))
+            VStack {
+                Text("Registro")
+                    .font(.largeTitle)
+                    .bold()
+                    .padding()
+                TextField("Usuario", text: $AccesoVM.user)
+                    .padding()
+                    .frame(width: 300, height: 50)
+                    .background(Color.black.opacity(0.05))
+                    .cornerRadius(10)
+                    .autocorrectionDisabled()
+                    .textInputAutocapitalization(.never)
+                    .submitLabel(.next)
+                    .focused($focus, equals: .user)
+                    .onSubmit {
+                        focus = .password
+                    }
+                TogglableSecureField("Contraseña",
+                                     secureContent: $AccesoVM.password,
+                                     onCommit: {
+                    guard !AccesoVM.password.isEmpty else { return }
+                })
+                .padding()
+                .frame(width: 300, height: 50)
+                .background(Color.black.opacity(0.05))
+                .cornerRadius(10)
+                .focused($focus, equals: .password)
+                .submitLabel(.go)
+                .onSubmit {
+                    Task {
+                        Login()
+                    }
+                }
+                ActionButton(state: $AccesoVM.buttonState, onTap: {
+                    Task {
+                        Login()
+                    }
+                }, backgroundColor: .blue)
+                .frame(width: 300, height: 50)
+            }
+        }
+    }
+    
+    func autenticarUsuario() async {
+        do {
+            try await AccesoVM.validarAcceso()
+        } catch {
+        }
+    }
+}
--- iOS App/Segnaventura/Segnaventura/SegnaventuraApp.swift
+++ iOS App/Segnaventura/Segnaventura/SegnaventuraApp.swift
@@ -18,6 +18,7 @@ struct SegnaventuraApp: App {
     @StateObject var palabraVideosVM = PalabraVideosViewModel()
     @StateObject var THVM = THViewModel()
     @StateObject var PVM = ProgresoViewModel()
+    @StateObject var AccesoVM = AccesoViewModel()
     
     init() {
         
@@ -52,7 +53,7 @@ struct SegnaventuraApp: App {
     
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            Acceso()
                 .environmentObject(VocabularioVM)
                 .environmentObject(VideoVM)
                 .environmentObject(fsm)
@@ -60,6 +61,7 @@ struct SegnaventuraApp: App {
                 .environmentObject(predictionStatus)
                 .environmentObject(THVM)
                 .environmentObject(PVM)
+                .environmentObject(AccesoVM)
         }
     }
 }
