bb561fe9da6e30d5b4a054fb41ce9eddb240d767
Christopher Pedraza <gabalixreborn@gmail.com>
2024-04-21 16:21:34 -0600

    Agregar endpoint para obtener los niveles

---
+++ backend/controllers/niveles.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+router.use(express.json());
+
+const { PrismaClient } = require("@prisma/client");
+const e = require("express");
+const prisma = new PrismaClient();
+
+router.get("/", async (req, res) => {
+    try {
+        const resultado = await prisma.nivel.findMany();
+        res.status(200).json(resultado);
+    } catch (err) {
+        res.status(500).json({ message: `${err}` });
+    }
+});
+
+module.exports = router;
+
+/*
+INSERT INTO public.isla(
+	nombre, modelo_general, modelo_especifico)
+	VALUES ('Isla', null, null);
+	
+INSERT INTO public.video_cuestionario(
+	nombre)
+	VALUES ('Video1');
+
+INSERT INTO public.nivel(
+	id_isla, id_video_cuestionario)
+	VALUES (1, 1);
+*/
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,14 +1,18 @@
-const express = require('express')
-const router = express.Router()
+const niveles = require("../controllers/niveles.js");
 
-router.use('/miembros', require('./routes_miembros.js'))
-router.use('/categorias', require('./routes_categorias.js'))
-router.use('/palabras', require('./routes_palabras.js'))
-router.use('/quiz', require('./routes_quiz.js'))
-router.use('/nivel', require('./routes_nivel.js'))
-router.use('/treasure', require('./routes_treasure_hunt.js'))
-router.use('/modelo', require('./routes_modelo_coml.js'))
-router.use('/videos', require('./routes_video_cuestionario.js'))
-router.use('/progreso', require('./routes_progreso.js'))
+const express = require("express");
+const router = express.Router();
 
-module.exports = router
+router.use("/miembros", require("./routes_miembros.js"));
+router.use("/categorias", require("./routes_categorias.js"));
+router.use("/palabras", require("./routes_palabras.js"));
+router.use("/quiz", require("./routes_quiz.js"));
+router.use("/nivel", require("./routes_nivel.js"));
+router.use("/treasure", require("./routes_treasure_hunt.js"));
+router.use("/modelo", require("./routes_modelo_coml.js"));
+router.use("/videos", require("./routes_video_cuestionario.js"));
+router.use("/progreso", require("./routes_progreso.js"));
+
+router.use("/niveles", niveles);
+
+module.exports = router;
