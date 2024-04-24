2fb532b46790fc106b1fa5636f0c3821106343fb
Christopher Pedraza <gabalixreborn@gmail.com>
2024-04-24 12:48:14 -0600

    Crear esqueleto para la página de parte de video

--- frontend/src/pages/ParteVideo.jsx
+++ frontend/src/pages/ParteVideo.jsx
@@ -5,10 +5,22 @@ import { useEffect, useState } from "react";
 import { get } from "src/utils/ApiRequests";
 
 // Enrutamiento
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 
 // Components
 import Navbar from "../components/Navbar";
+
+// NextUI components
+import { Button } from "@nextui-org/react";
+
+// Fontawesome icons
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// Plus icon
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+// Pencil icon
+import { faPencilAlt } from "@fortawesome/free-solid-svg-icons";
+// Left arrow icon
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 function ParteVideo() {
     // Obtener del url el id de la parte de video
@@ -17,16 +29,55 @@ function ParteVideo() {
     // Informacion de la parte
     const [data, setData] = useState([]);
 
+    // Historial de navegacion
+    const history = useHistory();
+
     useEffect(() => {
         get(`partesVideo/${id_parte}`).then((data) => {
             setData(data);
         });
     }, [id_parte]);
 
+    const handleReturn = () => {
+        history.goBack();
+    };
+
     return (
         <div>
             <Navbar />
-            {data.nombre}
+            <div className="max-w-3xl m-auto p-8">
+                <div className="flex items-center justify-between">
+                    <div className="flex items-center">
+                        <Button
+                            isIconOnly
+                            variant="light"
+                            onPress={handleReturn}
+                        >
+                            <FontAwesomeIcon icon={faArrowLeft} />
+                        </Button>
+                        <h2 className="text-3xl">{data.nombre}</h2>
+                    </div>
+                    <div className="flex items-center">
+                        <Button
+                            startContent={
+                                <FontAwesomeIcon icon={faPencilAlt} />
+                            }
+                            color="secondary"
+                            onPress={() => {}}
+                            className="mr-2"
+                        >
+                            Editar datos
+                        </Button>
+                        <Button
+                            startContent={<FontAwesomeIcon icon={faPlus} />}
+                            color="success"
+                            onPress={() => {}}
+                        >
+                            Pregunta
+                        </Button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
