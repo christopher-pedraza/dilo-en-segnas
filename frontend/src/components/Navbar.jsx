41d7430f155c0b0005d4050ab6eee49b7739d49d
Adrián Ramírez <adrian.rmz.587@gmail.com>
2024-08-20 14:44:12 -0600

    Created Accounts Page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,57 +1,38 @@
-import { NavLink } from "react-router-dom";
 import logo from "../assets/logo.png";
+import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
-    return (
-        <nav
-            className="flex items-center justify-between bg-black p-5"
-            style={{ background: "#080945" }}
+  return (
+    <nav
+      className="flex items-center justify-between bg-black p-5"
+      style={{ background: "#080945" }}
+    >
+      <div className="flex items-center">
+        <NavLink
+          to="/home"
+          className="flex items-center text-white text-4xl font-bold"
         >
-            <div className="flex items-center">
-                <img
-                    src={logo}
-                    alt="Logo Dilo en señas"
-                    width="60"
-                    height="60"
-                    className="mr-4"
-                />
-                <p className="text-4xl font-bold text-white">Dilo en Señas</p>
-            </div>
+          <img
+            src={logo}
+            alt="Logo Dilo en señas"
+            width="60"
+            height="60"
+            className="mr-4"
+          />
+          <p className="text-4xl font-bold text-white">Dilo en Señas</p>
+        </NavLink>
+      </div>
 
-            {/* <div className="flex">
-                <div className="mx-4">
-                    <NavLink
-                        to="/niveles"
-                        className="text-white text-xl hover:text-gray-300 hover:underline"
-                    >
-                        Niveles
-                    </NavLink>
-                </div>
-                <div className="mx-4">
-                    <NavLink
-                        to="/categories"
-                        className="text-white text-xl hover:text-gray-300 hover:underline"
-                    >
-                        Categorías
-                    </NavLink>
-                </div>
-                <div className="mx-4">
-                    <NavLink
-                        to="/quiz"
-                        className="text-white text-xl hover:text-gray-300 hover:underline"
-                    >
-                        Quiz
-                    </NavLink>
-                </div>
-                <div className="mx-4">
-                    <NavLink
-                        to="/videos"
-                        className="text-white text-xl hover:text-gray-300 hover:underline"
-                    >
-                        Videos
-                    </NavLink>
-                </div>
-            </div> */}
-        </nav>
-    );
+      <div className="flex">
+        <div className="mx-4">
+          <NavLink
+            to="/accounts"
+            className="text-white text-xl hover:text-gray-300 hover:underline"
+          >
+            Cuentas
+          </NavLink>
+        </div>
+      </div>
+    </nav>
+  );
 }
